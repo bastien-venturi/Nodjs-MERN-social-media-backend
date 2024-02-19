6e83447328980ffc3ea45b6d27c0b80948e95f1f
Venturi <bastien.aristide.venturi@gmail.com>
2024-02-19 13:40:50 +0100

    post

--- models/user.model.js
+++ models/user.model.js
@@ -46,8 +46,7 @@ const userSchema = new mongoose.Schema({
 },
 {
     timestamps: true
-}
-);
+});
 
 //play function before save into display: 'block',
 userSchema.pre('save', async function(next){
--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -1,22 +1,34 @@
 module.exports.signUpErrors = (err) => {
-  let errors = { pseudo: '', email: '', password: '' };  /* modifier les messages d'erreurs */
-    if (err.message.includes('pseudo')) 
-        errors.pseudo = "incorrect pseudo";
+  let errors = { pseudo: '', email: '', password: '' };/* modifier les messages d'erreurs */
+    
+  if (err.message.includes('pseudo')){ 
+        errors.pseudo = "incorrect pseudo"
+    }
 
-    if (err.message.includes('email'))
-        errors.email = "incorrect email";
+    if (err.message.includes('email')){        
+        errors.email = "incorrect email"
+    }
 
-    if (err.message.includes('password'))
-        errors.password = "password must be at least 6 characters long";
+    if (err.message.includes('password')){
+        errors.password = "password must be at least 6 characters long"
+    }
 
     if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('pseudo')) {
-        errors.email = "this pseudo is already registered";
+        errors.pseudo = "this pseudo is already registered"
+    }
 
     if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('email')) {
-        errors.email = "this email is already registered";
-    }
-    return errors;
+        errors.email = "this email is already registered"
     }
 
+    return errors;
+}
+
+module.exports.signInErrors = (err) => {
+    let errors = { email: '', password: '' };
+    
+    if (err.message.includes('email')) errors.email = "unknown email";
+    if (err.message.includes('password')) errors.password = "wrong password";
+    
     return errors;
 }
